e3638a97a58062ddf43448802ae90a2056debdab
Siudek <maciej.siudek@bettersolutions.pl>
2020-03-07 13:33:40 +0100

    add radio button change listener

--- src/main/java/com/bsk/Controllers/RadioButtonsController.java
+++ src/main/java/com/bsk/Controllers/RadioButtonsController.java
@@ -1,8 +1,11 @@
 package com.bsk.Controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import org.springframework.stereotype.Component;
 
@@ -29,5 +32,13 @@ public class RadioButtonsController implements Initializable {
         CBCRadio.setToggleGroup(toggleGroup);
         CFBRadio.setToggleGroup(toggleGroup);
         OFBRadio.setToggleGroup(toggleGroup);
+
+        toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                RadioButton chk = (RadioButton)newValue.getToggleGroup().getSelectedToggle();
+                System.out.println(chk.getText());
+            }
+        });
     }
 }
