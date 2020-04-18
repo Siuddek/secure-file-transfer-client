eff14bb5d84d2190a1513584ea0bdb907e95fae0
Siudek <maciej.siudek@bettersolutions.pl>
2020-04-18 16:31:23 +0200

    add messages for user, delete keys if exist

--- src/main/java/com/bsk/Services/KeyManagerService.java
+++ src/main/java/com/bsk/Services/KeyManagerService.java
@@ -2,6 +2,7 @@ package com.bsk.Services;
 
 import com.bsk.Configurations.KeyManagerConfiguration;
 import com.google.common.hash.Hashing;
+import javafx.scene.control.Alert;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,18 +22,39 @@ public class KeyManagerService {
 
     public void createRsaKeyPair(String password) throws NoSuchAlgorithmException {
         final String passwordHash = getEncryptedPassword(password); //TODO encode key files with user password
-        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-        keyGenerator.initialize(configuration.getKeyLength());
-        KeyPair keyPair = keyGenerator.generateKeyPair();
+        KeyPair keyPair = generateKeyPair();
         Key publicKey = keyPair.getPublic();
         Key privateKey = keyPair.getPrivate();
 
         try {
+            deletePreviousKeys();
             saveKeyToFile(configuration.getPublicKeyFolderPath(), publicKey, configuration.getPublicKeyCommentBegin(), configuration.getPublicKeyCommentEnd());
             saveKeyToFile(configuration.getPrivateKeyFolderPath(), privateKey, configuration.getPrivateKeyCommentBegin(), configuration.getPrivateKeyCommentEnd());
         } catch (IOException e) {
             e.printStackTrace(); //TODO handling
         }
+        showSuccessDialog();
+    }
+
+    private void showSuccessDialog() {
+        Alert successDialog = new Alert(Alert.AlertType.INFORMATION);
+        successDialog.setTitle("Success!");
+        successDialog.setHeaderText("RSA keys pair has been successfully generated");
+        successDialog.setContentText("Use typed password to decode keys");
+        successDialog.showAndWait();
+    }
+
+    private KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        keyGenerator.initialize(configuration.getKeyLength());
+        return keyGenerator.generateKeyPair();
+    }
+
+    private void deletePreviousKeys() {
+        File privateKeyFile = new File(configuration.getPrivateKeyFolderPath());
+        File publicKeyFile = new File(configuration.getPublicKeyFolderPath());
+        publicKeyFile.delete();
+        privateKeyFile.delete();
     }
 
     private void saveKeyToFile(String path, Key key, String beginComment, String endComment) throws IOException {
