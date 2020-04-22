e3a93e248c8e9da715a8ad39d57ef04282699a76
AdamWalentkowski <adam.walentkowski@gmail.com>
2020-04-22 22:30:41 +0200

    Change UI

---
+++ src/main/java/com/bsk/Controllers/TextAreaController.java
@@ -0,0 +1,14 @@
+package com.bsk.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TextAreaController {
+
+    @FXML
+    public Button sendTextButton;
+
+
+}
